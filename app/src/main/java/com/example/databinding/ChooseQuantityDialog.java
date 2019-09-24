fd962bdeec1229aad5250c609faae509e3a75cb9
perfect-code-ninja <muthomidenis62@gmail.com>
2019-09-24 15:19:43 +0300

    ui

--- app/src/main/java/com/example/databinding/ChooseQuantityDialog.java
+++ app/src/main/java/com/example/databinding/ChooseQuantityDialog.java
@@ -1,9 +1,9 @@
-package codingwithmitch.com.databindinggettingstarted;
+package com.example.databinding;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,9 +20,9 @@ public class ChooseQuantityDialog extends DialogFragment {
     // data binding
     DialogChooseQuantityBinding mBinding;
 
-    @Nullable
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mBinding = DialogChooseQuantityBinding.inflate(inflater);
         mBinding.listView.setOnItemClickListener(mOnItemClickListener);
         mBinding.closeDialog.setOnClickListener(mCloseDialogListener);
